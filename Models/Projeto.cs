7aba6be99794395d40b91e7b0daac6d193fac708
DeM4rcio <henriquemarcio170@gmail.com>
2025-04-08 12:46:52 -0300

    model

--- Models/Projeto.cs
+++ Models/Projeto.cs
@@ -11,30 +11,30 @@ public class Projeto
     public string NM_PROJETO {get;set;}
 
     [StringLength(300)]
-    public string GERENTE_PROJETO {get;set;}
+    public string? GERENTE_PROJETO {get;set;}
     
     [StringLength(300)]
-    public string SITUACAO {get;set;}
+    public string? SITUACAO {get;set;}
 
     [StringLength(300)]
-    public string UNIDADE {get;set;}
+    public string? UNIDADE {get;set;}
 
     [StringLength(300)]
-    public string NR_PROCESSO_SEI {get;set;}
+    public string? NR_PROCESSO_SEI {get;set;}
 
     [StringLength(300)]
-    public string NM_AREA_DEMANDANTE {get;set;}
+    public string? NM_AREA_DEMANDANTE {get;set;}
 
     [StringLength(100)]
     public string? ANO {get;set;}
 
     [StringLength(200)]
-    public string TEMPLATE {get;set;}
+    public string? TEMPLATE {get;set;}
 
-    public bool PROFISCOII {get;set;}
+    public bool? PROFISCOII {get;set;}
 
-    public bool PDTIC2427 {get;set;}
+    public bool? PDTIC2427 {get;set;}
 
-    public bool PTD2427 {get;set;}
+    public bool? PTD2427 {get;set;}
 
 }
